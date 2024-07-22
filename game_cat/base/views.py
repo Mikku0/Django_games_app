d39cd10dcaf074a98837a30104f8c8a7d991ad6d
Mikku0 <mikolaj080803@gmail.com>
2024-07-22 02:13:38 +0200

    added html templates

--- game_cat/base/views.py
+++ game_cat/base/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Adam'},
+    {'id': 2, 'name': 'Adam2'},
+    {'id': 3, 'name': 'Adam3'},
+    {'id': 4, 'name': 'Adam4'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)

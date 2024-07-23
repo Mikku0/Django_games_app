51c024d7c003d6467ea9df00c5c1891a539f481b
Mikku0 <mikolaj080803@gmail.com>
2024-07-23 17:01:21 +0200

    added CRUD

--- game_cat/base/views.py
+++ game_cat/base/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomFrom
 
 
 # rooms = [
@@ -21,3 +22,37 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def create_room(request):
+    form = RoomFrom()
+    if request.method == 'POST':
+        form = RoomFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomFrom(instance=room)
+
+    if request.method == 'POST':
+        form = RoomFrom(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj': room})

633d476144da1487100f9ca5f571ddc373863799
Mikku0 <mikolaj080803@gmail.com>
2024-07-24 22:50:10 +0200

    added login with flash messages

--- game_cat/base/views.py
+++ game_cat/base/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomFrom
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # rooms = [
@@ -11,6 +14,32 @@ from django.db.models import Q
 #     {'id': 3, 'name': 'Adam3'},
 #     {'id': 4, 'name': 'Adam4'},
 # ]
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is wrong')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
@@ -46,7 +75,7 @@ def create_room(request):
             form.save()
             return redirect('home')
 
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
 
@@ -69,4 +98,4 @@ def delete_room(request, pk):
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    return render(request, 'base/delete.html',{'obj': room})
+    return render(request, 'base/delete.html', {'obj': room})

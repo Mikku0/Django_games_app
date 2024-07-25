fc160fe2386dcf41f4906aaff55c23819e662e0e
Mikku0 <mikolaj080803@gmail.com>
2024-07-25 16:30:12 +0200

    added user registration

--- game_cat/base/views.py
+++ game_cat/base/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render, redirect
 from .models import Room, Topic
@@ -25,7 +26,7 @@ def login_page(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower
         password = request.POST.get('password')
 
         try:
@@ -51,8 +52,20 @@ def logout_user(request):
 
 
 def register_page(request):
-    page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
